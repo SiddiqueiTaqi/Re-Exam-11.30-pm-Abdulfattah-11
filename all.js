b2c2898b9434103eeb6a5061be4ccce31491b933
SiddiqueiTaqi <mdnasimuddiquei@gmail.com>
2022-02-15 00:17:59 +0530

    15-22

---
+++ all.js
@@ -0,0 +1,42 @@
+var students = ["Abdulfattah","Hamid","Usama"];
+
+var Abdulfattah ={
+    fullname:"Abdulfattah",
+    rollno: 11,
+    address:{
+        city:akkalkuwa,
+        dist: nsndurbar,
+        state:Maharashtra,
+        cuntry:India,
+    }
+
+};
+
+var Hamid ={
+    fullname:"Hamid",
+    rollno: 12,
+    address:{
+        city:akkalkuwa,
+        dist: nsndurbar,
+        state:Maharashtra,
+        cuntry:India,
+    }
+
+};
+
+var Usama ={
+    fullname:"Usama",
+    rollno: 13,
+    address:{
+        city:akkalkuwa,
+        dist: nsndurbar,
+        state:Maharashtra,
+        cuntry:India,
+    }
+
+};
+
+delete Hamid.address.dist;
+
+console.log("Hamid");
+
